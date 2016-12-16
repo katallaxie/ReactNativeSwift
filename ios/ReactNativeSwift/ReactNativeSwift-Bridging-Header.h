1b3adb0ddc71b050f58c7f3a51203be6f24244a0
Sebastian Döll <sebastian@pixelmilk.com>
2016-12-16 11:43:09 +0100

    ~ initial commit

---
+++ ios/ReactNativeSwift/ReactNativeSwift-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "RCTBridgeModule.h"
+#import "RCTBridge.h"
+#import "RCTEventEmitter.h"
+#import "RCTEventDispatcher.h"
+#import "RCTRootView.h"
+#import "RCTUtils.h"
+#import "RCTConvert.h"
+#import "RCTBundleURLProvider.h"
